2edfc578b05ecdcaa86d52e75b839c886cf432d4
kayconfig <odoleemma@gmail.com>
2023-03-27 19:35:40 +0100

    chore: create user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TYPEORM_ROOT_OPTIONS } from './app.constants';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync(TYPEORM_ROOT_OPTIONS),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],

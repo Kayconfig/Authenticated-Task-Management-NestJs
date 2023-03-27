190b4998436ddb2f4f6a9deeee04cbefd6c144c6
kayconfig <odoleemma@gmail.com>
2023-03-27 19:41:46 +0100

    chore: create user entity, register user entity with typeorm

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,13 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+}

64742e7a7213bcf0b0eb7b3889377fedcf99b617
kayconfig <odoleemma@gmail.com>
2023-03-27 19:22:10 +0100

    chore: setup env_config, postgres and docker compose to use env

---
+++ src/app.constants.ts
@@ -0,0 +1,16 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+export const TYPEORM_ROOT_OPTIONS: TypeOrmModuleAsyncOptions = {
+  useFactory: (config: ConfigService) => ({
+    type: 'postgres',
+    host: config.get('DB_HOST') || 'localhost',
+    port: +config.get('DB_PORT') || 5432,
+    username: config.getOrThrow('DB_USERNAME'),
+    password: config.getOrThrow('DB_PASSWORD'),
+    database: config.getOrThrow('DB_NAME'),
+    autoLoadEntities: true,
+    synchronize: config.get('NODE_ENV') === 'development',
+  }),
+  inject: [ConfigService],
+  imports: [ConfigModule],
+};
